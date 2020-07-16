92cd769c3c5e690a645e721df2446dabe8d8c33d
Markus W. Myrland <markusmyrland@hotmail.com>
2020-07-16 22:22:39 +0200

    Removed comments

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -20,15 +20,6 @@ function reducer(state, action) {
 function App() {
   const [state, dispatch] = useReducer(reducer, { palettes: [] })
 
-  // const removeCard = dispatch({ type: 'REMOVE_PALETTE', id: palette.id });
-
-  // const inputEl = useRef(null);
-  // const monochromeDark = useRef(null);
-  // const monochromeLight = useRef(null);
-  // const analogic = useRef(null);
-  // const complement = useRef(null);
-  // const anaComplement = useRef(null);
-
   useEffect(() => {
     const storage = window.localStorage.getItem('state');
     const jsonStorage = JSON.parse(storage);
@@ -61,48 +52,8 @@ function App() {
         <h1>InstantColors</h1>
         <h5>By Markus Willumstad Myrland</h5>
       </header>
-      <Form
-        getHex={getHex}
-      />
-      {/* <section className="App__form">
-        <form className="form__options">
-          <label htmlFor="#monoD">Dark Monochrome</label>
-          <input ref={monochromeDark} id="mono" type="checkbox" value="monochrome-dark" />
-          <label htmlFor="#monoL">Light Monochrome</label>
-          <input ref={monochromeLight} id="mono" type="checkbox" value="monochrome-light" />
-          <label htmlFor="#ana">Analogic</label>
-          <input ref={analogic} id="ana" type="checkbox" value="analogic" />
-          <label htmlFor="#comp">Complement</label>
-          <input ref={complement} id="comp" type="checkbox" value="complement" />
-          <label htmlFor="#anaComp">Analogic-Complement</label>
-          <input ref={anaComplement} id="anaComp" type="checkbox" value="analogic-complement" />
-        </form>
 
-        <form className="form__input" onSubmit={e => {
-          e.preventDefault();
-          if (monochromeDark.current.checked) {
-            getHex(inputEl.current.value, monochromeDark.current.value);
-            monochromeDark.current.checked = false;
-          } else if (monochromeLight.current.checked) {
-            getHex(inputEl.current.value, monochromeLight.current.value);
-            monochromeLight.current.checked = false;
-          } else if (complement.current.checked) {
-            getHex(inputEl.current.value, complement.current.value);
-            complement.current.checked = false;
-          } else if (analogic.current.checked) {
-            getHex(inputEl.current.value, analogic.current.value);
-            analogic.current.checked = false;
-          } else if (anaComplement.current.checked) {
-            getHex(inputEl.current.value, anaComplement.current.value);
-            anaComplement.current.checked = false;
-          } else {
-            getHex(inputEl.current.value);
-          }
-        }}>
-          <input ref={inputEl} type="text" placeholder="Enter a hex code" />
-          <input className="input__button" type="submit" value="Get Color Palette" />
-        </form>
-      </section> */}
+      <Form getHex={getHex} />
 
       <section className="App__content">
         {state.palettes.map(palette => {
